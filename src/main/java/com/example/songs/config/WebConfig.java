0bebf2bc6b6cc6893f51d35bb50e7ecdce96db77
aloaam <aloaam@gmail.com>
2023-12-28 18:09:50 +0100

    Stopped here, I'm trying to delete, with cascade remove, but I need to create bi-directionally relationships first.

--- src/main/java/com/example/songs/config/WebConfig.java
+++ src/main/java/com/example/songs/config/WebConfig.java
@@ -11,7 +11,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+
+
     }
 
 }
